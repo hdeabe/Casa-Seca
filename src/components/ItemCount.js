ef9d13cfed66ab558387de73c3d54fa25355c929
Hernán de Abelleyra <hernandeabelleyra@MacBook-Pro-de-Hernan.local>
2021-07-21 18:41:50 -0300

    clase5v2

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,42 +3,54 @@
 // - El número contador nunca puede superar el stock disponible
 // - De no haber stock el click no debe tener efecto y por ende no ejecutar el callback onAdd - Si hay stock al clickear el botón se debe ejecutar onAdd con un número que debe ser la cantidad seleccionada por el usuario.
 
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 
 function ItemCount() {
 
 
-    useEffect(() => {
 
-        if (initial > stock) {
-            
-            
-            alert(`No hay más stock`)}
-        // Actualiza el title de la página en cada click!
-        
-      })
-
-const [initial, onAdd] = useState(0);
-
-const stock = 5;
+const [count, setCount] = useState(0)
+const [stock, setStock] = useState(5)
 
 
+function decrementCount () {
+   
+   if (count > 0){
+    setCount(count-1)
+    setStock(stock+1)}
+}
+    
+function riseCount() {
+
+    if (stock > 0 ){
+    setCount(count+1)
+    setStock(stock-1)}
+}
 
 
+
+const onAdd = quantity => alert(`Agregaste ${count} productos al carrito`)
 
     // Desarrollar lógica
     return(
         
         <div>
-            <h1>{initial}</h1>
             
-            <div className="button-wrapper">
-            
-                <button onClick={()=>onAdd(initial-1)}>-</button>
+            <h1>Stock: {stock}</h1>
+            <h1>Comprar: {count}</h1>
+
+
             
 
-                <button onClick={()=>onAdd(initial+1)}>+</button>
+            <div className="button-wrapper">
+            
+            <button onClick={decrementCount}>-</button>
+
+            <button onClick={riseCount}>+</button>
+            
+            <button onClick={onAdd}>Confirmar compra</button>
+                
             </div>
 
         </div>
